85e7867ef3b659b30ede12dc343cd98624ec34c8
Artem Komlev <a.komlev.work@gmail.com>
2025-11-07 19:27:55 +0700

    Отредактировано подокно с вычисленными параметрами.

--- Widgets/SubWindow/sub_window_calculated_params.py
+++ Widgets/SubWindow/sub_window_calculated_params.py
@@ -6,14 +6,17 @@ from Widgets.SubWindow.sub_window_base import SubWindowBase
 class SubWindowCalculatedParams(SubWindowBase):
     def __init__(self):
         super().__init__()
-        self.label_names = [
-            "label_value_M1", "label_value_M2", "label_value_M3", "label_value_M4",
-            "label_value_M", "label_value_V1", "label_value_V2", "label_value_V3",
-            "label_value_V4", "label_value_p1", "label_value_p2"
-        ]
 
     def _setup_ui(self):
-        for label_name in self.label_names:
-            label = self.findChild(QLabel, label_name)
-            if label:
-                setattr(self, label_name, label)
+        self.label_value_M1: QLabel = self.findChild(QLabel, "label_value_M1")
+        self.label_value_M2: QLabel = self.findChild(QLabel, "label_value_M2")
+        self.label_value_M3: QLabel = self.findChild(QLabel, "label_value_M3")
+        self.label_value_M4: QLabel = self.findChild(QLabel, "label_value_M4")
+        self.label_value_M: QLabel = self.findChild(QLabel, "label_value_M")
+        self.label_value_V1: QLabel = self.findChild(QLabel, "label_value_V1")
+        self.label_value_V2: QLabel = self.findChild(QLabel, "label_value_V2")
+        self.label_value_V3: QLabel = self.findChild(QLabel, "label_value_V3")
+        self.label_value_V4: QLabel = self.findChild(QLabel, "label_value_V4")
+        self.label_value_p1: QLabel = self.findChild(QLabel, "label_value_p1")
+        self.label_value_p2: QLabel = self.findChild(QLabel, "label_value_p2")
+
